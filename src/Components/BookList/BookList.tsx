5c8fb28f0e00a61fd86854d2dc0f762e678a34db
Vasanti-Vaghela <vasantivaghela2812@gmail.com>
2025-03-14 03:08:56 +0530

    create book card component and memoize the booklist if it is not changed

--- src/Components/BookList/BookList.tsx
+++ src/Components/BookList/BookList.tsx
@@ -1,52 +1,22 @@
 import "./BookList.css";
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { addFavorite } from "../../store/favorites-slice";
+import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
+import BookCard from "../BookCard/BookCard";
+import { useMemo } from "react";
 
 const BookList: React.FC = () => {
-  const dispatch = useDispatch();
   const { books, status, error } = useSelector(
     (state: RootState) => state.books
   );
+  const searchedBooks = useMemo(() => books, [books]); //Reduces unnecessary renders of components that use books
+
   return (
     <div>
       {status === "loading" && <p>Loading...</p>}
       {error && <p className="error-message">{error}</p>}
       <div className="book-grid">
-        {books.length > 0
-          ? books.map((book) => (
-              // <Link to={`/book/${book.id}`}>
-              <div key={book.id} className="book-card">
-                {book.image && (
-                  <Link to={`/book/${book.id}`}>
-                    <img
-                      className="book-image"
-                      src={book.image}
-                      alt={book.title}
-                    />
-                  </Link>
-                )}
-                <div className="book-info">
-                  <h3 className="book-title">{book.title}</h3>
-                  <p className="book-author">
-                    {book.authors?.join(", ") || "Unknown Author"}
-                  </p>
-                  <div className="book-description">
-                    {book.description || "No Date Available"}
-                  </div>
-                </div>
-                <div className="add-favorite">
-                  <button
-                    className="add-favorite-btn"
-                    onClick={() => dispatch(addFavorite(book))}
-                  >
-                    ❤️ Add to Favorites
-                  </button>
-                </div>
-              </div>
-              // </Link>
-            ))
+        {searchedBooks.length > 0
+          ? books.map((book) => <BookCard key={book.id} book={book} />)
           : status != "loading" && !error && <p>No books found.</p>}
       </div>
     </div>
